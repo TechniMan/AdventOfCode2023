ba3693d09009435eb2b1519c9d72e74eeb0da527
Will Thomas <technimanx@gmail.com>
2023-12-01 22:59:20 +0000

    Completed day 1 part 2

--- solutions/Day01.cs
+++ solutions/Day01.cs
@@ -1,7 +1,22 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode2023;
 
 class Day01 : ISolution
 {
+    private static List<string> Indices = new() {
+        "zero", // unused; but buffers up the indices :)
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine"
+    };
+
     /// <summary>
     /// Read all lines of input.
     /// Find the calibration value of each one.
@@ -14,14 +29,17 @@ class Day01 : ISolution
         var input = GetInput("01");
 
         // 2 - find answer
-        var calibrationValues = new List<int>();
+        var answer1 = 0;
+        var answer2 = 0;
+        var n = 0;
         foreach (var line in input)
         {
+            // PART 1
             int value = 0;
             int digit = 0;
             foreach (var c in line)
             {
-                if (int.TryParse(c.ToString(), out int n))
+                if (int.TryParse(c.ToString(), out n))
                 {
                     digit = n;
                     // if first digit found, add as tens
@@ -34,10 +52,21 @@ class Day01 : ISolution
             // at the end, add the final digit we found
             // this could include the only digit of the line, e.g. if the only digit on the line is '7' then the answer is '77'
             value += digit;
-            calibrationValues.Add(value);
+            answer1 += value;
+
+            // PART 2
+            value = 0;
+            var match = Regex.Match(line, "(\\d|one|two|three|four|five|six|seven|eight|nine)").Value;
+            var v = int.TryParse(match, out n) ? n : Indices.IndexOf(match);
+            value += v * 10;
+            match = Regex.Match(line, "(\\d|one|two|three|four|five|six|seven|eight|nine)", RegexOptions.RightToLeft).Value;
+            v = int.TryParse(match, out n) ? n : Indices.IndexOf(match);
+            value += v;
+            answer2 += value;
         }
 
         // 3 - output solution
-        Console.WriteLine($"Part 1 answer: {calibrationValues.Sum()}");
+        Console.WriteLine($"Part 1 answer: {answer1}");
+        Console.WriteLine($"Part 2 answer: {answer2}");
     }
 }
