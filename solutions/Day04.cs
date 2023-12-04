03d549029340ed8bd93b4f9a4cb9bcab62e97bf2
Will Thomas <technimanx@gmail.com>
2023-12-04 22:24:09 +0000

    Completed day 4 part 2

--- solutions/Day04.cs
+++ solutions/Day04.cs
@@ -5,9 +5,15 @@ class Day04 : ISolution {
     var input = GetInput("04");
 
     var answer1 = 0;
+    var copies = new List<int>();
+    // initialise copies - we start with 1 of each
+    for (var c = 0; c < input.Count; ++c) {
+      copies.Add(1);
+    }
+
     // foreach line/ticket/game
-    for (var gameId = 1; gameId < input.Count; ++gameId) {
-      var line = input[gameId - 1];
+    for (var game = 0; game < input.Count; ++game) {
+      var line = input[game];
       // cut off "Game X:"
       line = line.Split(": ")[1];
       // split the winning numbers from those on the ticket
@@ -17,13 +23,20 @@ class Day04 : ISolution {
       // determine any matches
       var numMatches = ticketNumbers.Count(winningNumbers.Contains);
 
-      // determine points
+      // PART 1 - determine points
       var points = numMatches > 0 ? (numMatches > 1 ? (2 << (numMatches - 2)) : 1) : 0;
       answer1 += points;
 
-      // Console.WriteLine($"G:{gameId} - M:{numMatches} - P:{points} - A:{answer1}");
+      Console.WriteLine($"G:{game + 1} - C:{copies[game]} - M:{numMatches} - P:{points} - A:{answer1}");
+
+      // PART 2 - ADD COPIES
+      for (var futureGame = game + 1; futureGame < game + numMatches + 1; ++futureGame) {
+        copies[futureGame] += copies[game];
+      }
     }
 
     Console.WriteLine($"Part 1: {answer1}");
+    var answer2 = copies.Sum();
+    Console.WriteLine($"Part 2: {answer2}");
   }
 }
