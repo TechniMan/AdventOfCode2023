d05abafda9bc89a8a2857ae8202e8ad6f5f5b00a
Will Thomas <technimanx@gmail.com>
2023-12-06 00:51:03 +0000

    Completed day 5 part 1

--- Program.cs
+++ Program.cs
@@ -1,24 +1,24 @@
-﻿namespace AdventOfCode2023 {
-    internal class Program {
-        static Dictionary<string, Action> Days = new() {
-            { "1", Day01.Solve },
-            { "2", Day02.Solve },
-            { "3", Day03.Solve },
-            { "4", Day04.Solve },
-            { "5", Day05.Solve },
-        };
+﻿namespace AdventOfCode2023;
 
-        static void Main(string[] args) {
-            if (args.Length != 1) {
-                Console.WriteLine($"Invalid args: {string.Join(", ", args)}.");
-                return;
-            }
+internal class Program {
+    static Dictionary<string, Action> Days = new() {
+        { "1", Day01.Solve },
+        { "2", Day02.Solve },
+        { "3", Day03.Solve },
+        { "4", Day04.Solve },
+        { "5", Day05.Solve },
+    };
 
-            if (!Days.ContainsKey(args[0])) {
-                Console.WriteLine($"Unrecognised puzzle ID: {args[0]}.");
-            }
-
-            Days[args[0]]();
+    static void Main(string[] args) {
+        if (args.Length != 1) {
+            Console.WriteLine($"Invalid args: {string.Join(", ", args)}.");
+            return;
         }
+
+        if (!Days.ContainsKey(args[0])) {
+            Console.WriteLine($"Unrecognised puzzle ID: {args[0]}.");
+        }
+
+        Days[args[0]]();
     }
 }
--- solutions/Day05.cs
+++ solutions/Day05.cs
@@ -1,16 +1,123 @@
 namespace AdventOfCode2023;
 
-class Day05 : ISolution {
-  public static void Solve() {
-    // process input
-    var input = GetInput("05");
-    var initialSeeds = input[0].Split(": ")[1].Split(" ").Select(s => int.Parse(s)).ToList();
+class Mapping
+{
+    public readonly struct LongRange(long start, long end)
+    {
+        public readonly long Start = start;
+        public readonly long End = end;
 
-    // map reading:
-    // destinationStart sourceStart rangeLength
+        public bool Contains(long value) => value >= Start && value <= End;
+        public long IndexOf(long value) => value - Start;
+        public long ValueOf(long index) => Start + index;
+    }
 
-    // find answer
-    var answer1 = 0;
-    Console.WriteLine($"Part 1: {answer1}");
-  }
+    /// <summary>
+    /// key: SOURCE range
+    /// value: DESTINATION range
+    /// </summary>
+    public readonly Dictionary<LongRange, LongRange> Ranges = [];
+
+    public Mapping() { }
+
+    /// <summary>
+    /// Transform the given value through the map.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public long MappingFor(long value) {
+        foreach (var range in Ranges) {
+            // if in 'source' range
+            if (range.Key.Contains(value)) {
+                // find value from 'destination' range
+                return range.Value.ValueOf(range.Key.IndexOf(value));
+            }
+        }
+        // by default, return itself
+        return value;
+    }
+}
+
+class Day05: ISolution {
+    public static void Solve() {
+        // process input
+        var input = GetInput("05");
+        var initialSeeds = input[0].Split(": ")[1].Split(" ").Select(long.Parse).ToList();
+
+        // map reading:
+        // destinationStart sourceStart rangeLength
+
+        #region create the maps
+        var seedToSoilMap = new Mapping();
+        for (var l = 3; l < 32; ++l) {
+            var split = input[l].Split(' ').Select(long.Parse).ToList();
+            var sourceRange = new Mapping.LongRange(split[1], split[1] + split[2]);
+            var destinationRange = new Mapping.LongRange(split[0], split[0] + split[2]);
+            seedToSoilMap.Ranges.Add(sourceRange, destinationRange);
+        }
+        var soilToFertiliserMap = new Mapping();
+        for (var l = 34; l < 53; ++l) {
+            var split = input[l].Split(' ').Select(long.Parse).ToList();
+            var sourceRange = new Mapping.LongRange(split[1], split[1] + split[2]);
+            var destinationRange = new Mapping.LongRange(split[0], split[0] + split[2]);
+            soilToFertiliserMap.Ranges.Add(sourceRange, destinationRange);
+        }
+        var fertiliserToWaterMap = new Mapping();
+        for (var l = 55; l < 97; ++l) {
+            var split = input[l].Split(' ').Select(long.Parse).ToList();
+            var sourceRange = new Mapping.LongRange(split[1], split[1] + split[2]);
+            var destinationRange = new Mapping.LongRange(split[0], split[0] + split[2]);
+            fertiliserToWaterMap.Ranges.Add(sourceRange, destinationRange);
+        }
+        var waterToLightMap = new Mapping();
+        for (var l = 99; l < 116; ++l) {
+            var split = input[l].Split(' ').Select(long.Parse).ToList();
+            var sourceRange = new Mapping.LongRange(split[1], split[1] + split[2]);
+            var destinationRange = new Mapping.LongRange(split[0], split[0] + split[2]);
+            waterToLightMap.Ranges.Add(sourceRange, destinationRange);
+        }
+        var lightToTemperatureMap = new Mapping();
+        for (var l = 119; l < 132; ++l) {
+            var split = input[l].Split(' ').Select(long.Parse).ToList();
+            var sourceRange = new Mapping.LongRange(split[1], split[1] + split[2]);
+            var destinationRange = new Mapping.LongRange(split[0], split[0] + split[2]);
+            lightToTemperatureMap.Ranges.Add(sourceRange, destinationRange);
+        }
+        var temperatureToHumidityMap = new Mapping();
+        for (var l = 134; l < 144; ++l) {
+            var split = input[l].Split(' ').Select(long.Parse).ToList();
+            var sourceRange = new Mapping.LongRange(split[1], split[1] + split[2]);
+            var destinationRange = new Mapping.LongRange(split[0], split[0] + split[2]);
+            temperatureToHumidityMap.Ranges.Add(sourceRange, destinationRange);
+        }
+        var humidityToLocationMap = new Mapping();
+        for (var l = 146; l < 190; ++l) {
+            var split = input[l].Split(' ').Select(long.Parse).ToList();
+            var sourceRange = new Mapping.LongRange(split[1], split[1] + split[2]);
+            var destinationRange = new Mapping.LongRange(split[0], split[0] + split[2]);
+            humidityToLocationMap.Ranges.Add(sourceRange, destinationRange);
+        }
+        #endregion
+
+        // now use the maps
+        var lowestLocation = long.MaxValue;
+        foreach (var seed in initialSeeds) {
+            // follow this number through each map until we reach the end
+            var soil = seedToSoilMap.MappingFor(seed);
+            var fertiliser = soilToFertiliserMap.MappingFor(soil);
+            var water = fertiliserToWaterMap.MappingFor(fertiliser);
+            var light = waterToLightMap.MappingFor(water);
+            var temperature = lightToTemperatureMap.MappingFor(light);
+            var humidity = temperatureToHumidityMap.MappingFor(temperature);
+            var location = humidityToLocationMap.MappingFor(humidity);
+
+            if (location < lowestLocation) {
+                lowestLocation = location;
+            }
+        }
+
+        // find answer
+        var answer1 = lowestLocation;
+        Console.WriteLine($"Part 1: {answer1}");
+    }
 }
